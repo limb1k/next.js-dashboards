23ff4df2ea0aea28359760aeec6d8d90b3f2340b
limb1k <arcmik97@gmail.com>
2024-04-04 17:33:13 +0300

    added rest of the course's code

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,50 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace} = useRouter();
+
+  // function debounce(func:any, ms:number) {
+  //   let timeout:any;
+  //   return (...args: any) => {
+  //     clearTimeout(timeout);
+  //     timeout = setTimeout(() => func(...args), ms);
+  //   };
+  // }
+
+  // const handleSearch = debounce(function handleSearch(term: string) {
+  //   console.log(term);
+  //   const params = new URLSearchParams(searchParams);
+    
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`);
+  // }, 300);
+
+
+
+  const handleSearch = useDebouncedCallback(function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +53,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
